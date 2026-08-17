bd25f29240cbd0e31bf8f268d8c014859f59cf4f
VictorIndiaCharlie <victor.masca21@gmail.com>
2026-08-18 01:43:35 +0300

    Update greet.sh

--- bash_assignments/greet.sh
+++ bash_assignments/greet.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # greet.sh - Greeting script with date
 
-# Check if an argument was provided
-if [ $# -eq 0 ]; then
-    echo "Usage: ./greet.sh <name>"
-    exit 1
-fi
-
-# Store the first argument as the name
 name=$1
-
-# Print the greeting with current date
 echo "Hello $name! Today is $(date)"
